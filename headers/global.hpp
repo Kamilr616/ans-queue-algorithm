32d673be9754717c61f25ef457946435f5750d24
Kamil Rataj <kamilr616@gmail.com>
2022-12-19 14:10:45 +0100

    first queue

--- headers/global.hpp
+++ headers/global.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
 #include <cmath>
+#include <array>
 
 #define MAX 500 // Maximum number of digits in output
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "headers/global.hpp"
 #include "Factorial.cpp"
+#include "Queue1.cpp"
 
 using namespace std;
 
@@ -7,14 +8,24 @@ using namespace std;
 int main()
 {
     int n1;
-    cout << " Podaj n (silnia) >> ";
-    cin >> n1;
+    n1 = 15;
     Factorial A(n1);
     A.printFactorial();
     A.printFactorialExp();
     unsigned long long A0;
     A0 = A.getFactorial();
     cout << "\n getFactorial() >> " << A0;
+
+    Queue1 q;
+    q.Enqueue(1);
+    q.Enqueue(2);
+    q.Enqueue(3);
+    std::cout << "Front: " << q.Front() << std::endl;
+    std::cout << "Back: " << q.Back() << std::endl;
+    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
+    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
+    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
+    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
 
     return 0;
 }
