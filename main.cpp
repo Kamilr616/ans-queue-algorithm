5de984438a87abcd7424ce21cb592af3854818cf
Dawid <122745469+EagleBlood@users.noreply.github.com>
2023-01-15 20:13:30 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,11 @@
-#include "headers/global.hpp"
-#include "Factorial.cpp"
-#include "Queue1.cpp"
+#include "mainwindow.h"
 
-using namespace std;
+#include <QApplication>
 
-
-int main()
+int main(int argc, char *argv[])
 {
-    int n1;
-    n1 = 15;
-    Factorial A(n1);
-    A.printFactorial();
-    A.printFactorialExp();
-    unsigned long long A0;
-    A0 = A.getFactorial();
-    cout << "\n getFactorial() >> " << A0;
-
-    Queue1 q;
-    q.Enqueue(1);
-    q.Enqueue(2);
-    q.Enqueue(3);
-    std::cout << "\n\nFront: " << q.Front() << std::endl;
-    std::cout << "Back: " << q.Back() << std::endl;
-    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
-    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
-    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
-    std::cout << "Dequeue: " << q.Dequeue() << std::endl;
-
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
