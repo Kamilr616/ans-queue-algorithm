8faa6dd30f1ad27e6739add0fedebb69f1120474
EagleBlood <35221@student.pwsztar.edu.pl>
2023-01-27 15:50:54 +0100

    Comit test

---
+++ Steps.cpp
@@ -0,0 +1,25 @@
+#include "Steps.hpp"
+
+
+
+Steps::Steps(double _lambda, double _mikro, int _m, int _N)
+{
+    this->lambda = _lambda;
+    this->mikro = _mikro;
+    this->m = _m;
+    this->N = _N;
+
+    step1();
+
+}
+
+void Steps::step1()
+{
+    int l = this->lambda;
+
+}
+
+Steps::~Steps(){
+
+
+}
